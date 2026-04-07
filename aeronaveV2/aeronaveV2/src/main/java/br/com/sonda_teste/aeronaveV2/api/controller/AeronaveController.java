a703aa6943410dc132814194d42da6b27de736e4
Lucas Rocha <lucasrochadnsx@gmail.com>
2026-04-06 23:23:45 -0300

    Commit inicial

--- aeronaveV2/aeronaveV2/src/main/java/br/com/sonda_teste/aeronaveV2/api/controller/AeronaveController.java
+++ aeronaveV2/aeronaveV2/src/main/java/br/com/sonda_teste/aeronaveV2/api/controller/AeronaveController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+
 @RestController
 @RequestMapping("/aeronaves")
 @RequiredArgsConstructor
